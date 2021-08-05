e8ae9f313ee29c2371edfa762c50ae7e69ac9ed4
NHS <nhsouissi@fininfosolutions.com>
2021-08-05 12:27:20 +0100

    generate entities

--- src/main/java/com/fininfo/config/CacheConfiguration.java
+++ src/main/java/com/fininfo/config/CacheConfiguration.java
@@ -48,6 +48,11 @@ public class CacheConfiguration {
             createCache(cm, com.fininfo.domain.User.class.getName());
             createCache(cm, com.fininfo.domain.Authority.class.getName());
             createCache(cm, com.fininfo.domain.User.class.getName() + ".authorities");
+            createCache(cm, com.fininfo.domain.Blog.class.getName());
+            createCache(cm, com.fininfo.domain.Post.class.getName());
+            createCache(cm, com.fininfo.domain.Post.class.getName() + ".tags");
+            createCache(cm, com.fininfo.domain.Tag.class.getName());
+            createCache(cm, com.fininfo.domain.Tag.class.getName() + ".posts");
             // jhipster-needle-ehcache-add-entry
         };
     }
